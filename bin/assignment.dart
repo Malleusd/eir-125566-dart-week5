42dc2e15fc9c8f7b70ca1dde440d272a984ad15c
Malleusd <malleusdraconia63@gmail.com>
2023-06-16 16:05:42 +0700

    first app

---
+++ bin/assignment.dart
@@ -0,0 +1,12 @@
+
+void main(){
+  int n1 = 10;
+  int n2 = 5;
+  print("Example of Assignmet opertors");
+  print("in+=n2 = ${n1+=n2}");
+  print("n1-n2 = ${n1-n2}");
+  print("n1*n2 = ${n1*n2}");
+  print("n1/n2 = ${n1/n2}");
+  print("n1 /n2 = ${n1~/n2}");
+  print("n1%n2 = ${n1%n2}");
+}
